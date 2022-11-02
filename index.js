65c0828c7a935a73fec8482a65e194e32085dc00
jasimahmedq2 <jasimahmed90852417>
2022-11-02 06:50:08 +0600

    make-admin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ServiceRouter = require('./Routes/Service.Route')
 const app = express()
@@ -14,13 +15,43 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vugvxkr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT  (req, res, next){
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unAuthorized access' })
+  }
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.JSON_WEB_SECRET_KEY, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+    req.decoded = decoded
+    next()
+  })
+
+}
+
 async function run() {
+
 
   try {
     await client.connect()
     const serviceCollection = client.db('creative').collection('service')
     const reviewCollection = client.db('creative').collection('review')
     const userCollection = client.db('creative').collection('user')
+
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+   
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      console.log(requesterAccount)
+      if (requesterAccount.role === 'admin') {
+        next();
+      }
+      else {
+        res.status(403).send({ message: 'forbidden' });
+      }
+    }
 
     // app.use('/service', ServiceRouter)
     app.get('/service', async (req, res) => {
@@ -37,18 +68,46 @@ async function run() {
       res.send(result)
     });
 
-    app.put('/user/:email', async(req, res) => {
+    // add review
+    app.post('/review', async (req, res) => {
+      const reviewData = req.body;
+      const result = await reviewCollection.insertOne(reviewData)
+      res.send(result)
+    })
+
+    app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
-      const filter = {email: email}
+      const filter = { email: email }
       const options = { upsert: true };
       const updateEmail = {
-       $set: user
-     };
-     const result = await userCollection.updateOne(filter, updateEmail, options);
-     res.send(result)  
+        $set: user
+      }
+      const result = await userCollection.updateOne(filter, updateEmail, options);
+
+      const token = jwt.sign({ email: email }, process.env.JSON_WEB_SECRET_KEY, { expiresIn: '1h' })
+      res.send({ result, token })
+
+    });
+
+    app.put('/user/admin/:email', verifyJWT,  async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // add service
+    app.post('/service', async (req, res) => {
+      const inserted = req.body
+      const result = await serviceCollection.insertOne(inserted)
+      res.send(result)
     })
   }
+
   finally {
 
   }
